f18407852c4754eb63b2c0e5743ab633d904fc53
Eryk Kleczewski <MrNikoZero@gmail.com>
2021-06-28 16:34:33 +0200

    task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Models\Room;
 
 class TaskController extends Controller
 {
@@ -12,9 +13,22 @@ class TaskController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($roomKey)
     {
-        //
+        try {
+            return Room::where('room_key', $roomKey)->firstOrFail()->tasks;
+        } catch (\Throwable $th) {
+            $room = Room::create([
+                'room_key' => $roomKey,
+            ]);
+
+            $room->tasks()->create([
+                'title' => 'Twoje Pierwsze Zadanie',
+                'description' => 'Dodaj kolejne!',
+            ]);
+
+            return response($room->tasks, 201);
+        }
     }
 
     /**
@@ -23,9 +37,21 @@ class TaskController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $roomKey)
     {
-        //
+        $room = Room::where('room_key', $roomKey)->firstOrFail();
+
+        $request->validate([
+            'name' => 'required|max:120',
+            'description' => 'nullable|max:600',
+        ]);
+
+        $input = $request->all();
+
+        return $room->tasks()->create([
+            'title' => $input['name'],
+            'description' => $input['description'],
+        ]);
     }
 
     /**
@@ -46,9 +72,16 @@ class TaskController extends Controller
      * @param  \App\Models\Task  $task
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Task $task)
+    public function update(Request $request, $roomKey, $taskId)
     {
-        //
+        $room = Room::where('room_key', $roomKey)->firstOrFail();
+
+        $input = $request->boolean('isCompleted');
+
+        $task = $room->tasks()->findOrFail($taskId);
+        $task->is_completed = $input;
+
+        $task->save();
     }
 
     /**
@@ -57,8 +90,10 @@ class TaskController extends Controller
      * @param  \App\Models\Task  $task
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Task $task)
+    public function destroy($roomKey, $taskId)
     {
-        //
+        $room = Room::where('room_key', $roomKey)->firstOrFail();
+
+        $room->tasks()->findOrFail($taskId)->delete();
     }
 }
